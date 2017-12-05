669df39a2761007bd26838fc3144dbfc722a6627
engtentan <tanengten@gmail.com>
2017-12-05 22:56:39 +1100

    Added mpeg dash support

--- app.js
+++ app.js
@@ -178,14 +178,8 @@ function getOptions(clientReq, callback) {
 		return callback(null, options);
 }
 
-function serveStaticFiles(clientReq, clientRes, options) {
-	// parse URL
-	const parsedUrl = url.parse(clientReq.url);
-	// extract URL path
-	let pathname = `.${parsedUrl.pathname}`;
-	// based on the URL path, extract the file extention. e.g. .js, .doc, ...
+function sendFile(pathname, clientRes) {
 	const ext = path.parse(pathname).ext;
-	// maps file extention to MIME typere
 	const map = {
 		'.ico': 'image/x-icon',
 		'.html': 'text/html',
@@ -198,36 +192,69 @@ function serveStaticFiles(clientReq, clientRes, options) {
 		'.mp3': 'audio/mpeg',
 		'.svg': 'image/svg+xml',
 		'.pdf': 'application/pdf',
-		'.doc': 'application/msword'
+		'.doc': 'application/msword',
+		'.mpd': 'application/dash+xml',	// mpeg dash manifest
+		'.chk': 'video/webm'			// mpeg dash chunk, vp9 encoded in webm encapsulation
 	};
+	
+	// if is a directory search for index file matching the extention
+	if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
 
-	fs.exists(pathname, function (exist) {
-		if(!exist) {
-			// if the file is not found, return 404
-			clientRes.statusCode = 404;
-			clientRes.end(`File ${pathname} not found!`);
-			return;
+	// read file from file system
+	fs.readFile(pathname, function(err, data){
+		if(err){
+			clientRes.statusCode = 500;
+			clientRes.end(`Error getting the file: ${err}.`);
+		} else {
+			// if the file is found, set Content-type and send data
+			clientRes.setHeader('Content-type', map[ext] || 'text/plain' );
+			clientRes.end(data);
 		}
+	});
+}		
 
-		// if is a directory search for index file matching the extention
-		if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
-
-		// read file from file system
-		fs.readFile(pathname, function(err, data){
-			if(err){
-				clientRes.statusCode = 500;
-				clientRes.end(`Error getting the file: ${err}.`);
-			} else {
-				// if the file is found, set Content-type and send data
-				clientRes.setHeader('Content-type', map[ext] || 'text/plain' );
-				clientRes.end(data);
+function serveStaticFiles(clientReq, clientRes, options) {
+	// parse URL
+	const parsedUrl = url.parse(clientReq.url);
+	// extract URL path
+	let pathname = `.${parsedUrl.pathname}`;
+	// based on the URL path, extract the file extention. e.g. .js, .doc, ...
+	const ext = path.parse(pathname).ext;
+	
+	fs.exists(pathname, function (exist) {
+		if (!exist) {
+			var seps = pathname.split('/') || [];
+			// if the file is not found, first check if this is a request for dash manifest
+			// ./www/webm_live/<camera_name>/manifest.mpd
+			if (ext === '.mpd' && seps.length === 5) {
+				var camera = cameras.find(function(camera) {
+					return (seps[3] === camera.name);
+				}, options);
+				if (camera) {
+					return camera.generateDashManifest(clientReq, clientRes, pathname, function(error) {
+						if (error) {
+							clientRes.statusCode = 500;
+							return clientRes.end(error.message);
+						}
+						// Dash manifest ready
+						return sendFile(pathname, clientRes);
+					});
+				}
 			}
+			// return 404
+			clientRes.statusCode = 404;
+			return clientRes.end(`File ${pathname} not found!`);
+		}
+		cameras.forEach(function(camera) {
+			camera.checkDashRequest(clientReq); // update dash request flag 
 		});
+		sendFile(pathname, clientRes);
 	});
 }
 
 class EsCamG02 {
 	constructor(name, hostname, port, username, password) {
+		this.className = this.constructor.name;
 		this.name = name;
 		this.options = {
 			method: 'GET',
@@ -278,6 +305,16 @@ class EsCamG02 {
 		}
 	}
 	
+	// vp9/webm live stream. (this is not MPEG DASH. see below)
+	// simple live streaming of vp9-encoded webm-encapsulated stream via http. clients simply use the
+	// html <video> tag to play the feed.
+	// this method is called in response to api get 'stream' subresource. the client request object
+	// is appended to the list of clients array. if this is the first client then an ffmpeg process is run
+	// to read rtsp stream from camera and transcode the video/audio feed into vp9/vorbis. this output
+	// is broadcasted to all clients. (TODO: does not really work... clients[2..n] are unable to decode.
+	// i think this is due to the missing header that only occur at the beginning of the stream. i've tried
+	// to save the first 2 buffers and prepend these to every new connections but it doesn't work.. perhaps
+	// MPEG DASH is the only solution to achieve vp9 live streaming. see below)
 	addStreamListener(client) {
 		var self = this;
 		console.log('EsCamG02(' + this.name + ') client connected (' + client.socket.remoteAddress + ':' + client.socket.remotePort + ')');
@@ -318,12 +355,10 @@ class EsCamG02 {
 				'4', '-qmax', '48', '-error-resilient', '1', '-codec:v', 'libvpx-vp9', '-c:a', 'libopus', '-r', '23', '-f', 'webm', '-'], {
 				detached: false
 			});
-			/*this.stream = child_process.spawn("ffmpeg", ['-i','.\\www\\cam\\raw.mp4', '-f', 'webm', '-vcodec', 'libvpx', '-r', '29.97', '-vb', '250k', '-acodec',
-				'libvorbis', '-ab', '64k', '-threads', '8', '-'], {
-				detached: false
-			});*/
 			console.log('EsCamG02(' + this.name + ') first client. stream started pid(' + this.stream.pid + ')') ;
 			this.stream.stdout.on('data', function(data) {
+				// save first 2 buffers as i believe these contain the headers. prepend these to all subsequent connections that
+				// join mid-stream. doesn't work :/
 				if (!self.streamHeaders)
 					self.streamHeaders = [];
 				if (self.streamHeaders.length < 2)
@@ -365,6 +400,132 @@ class EsCamG02 {
 			client.end();
 		});
 		this.clients = [];
+	}
+	
+	deleteFolderRecursive(path) {
+		if (fs.existsSync(path)) {
+			fs.readdirSync(path).forEach(function(file, index) {
+				var curPath = path + "/" + file;
+				if (fs.lstatSync(curPath).isDirectory()) { // recurse
+					this.deleteFolderRecursive(curPath);
+				} else { // delete file
+					fs.unlinkSync(curPath);
+				}
+			});
+			fs.rmdirSync(path);
+		}
+	}
+	
+	// Mpeg DASH -- Dynamic Adaptive Streaming over HTTP
+	// this method is called in response to a request for mpeg dash manifest that does not exist yet.
+	// this camera instance has been identified as the one to generate the manifest and webm chunks.
+	// two ffmpeg processes will be run. the first one runs forever until explicitly killed. this first
+	// ffmpeg process generates the webm chunks and header files. then a second ffmpeg process runs that
+	// uses the header files generated by the first ffmpeg process to generate an mpeg dash manifest.
+	generateDashManifest(clientReq, clientRes, pathname, callback) {
+		var self = this;
+		if (self.dashProcess) {
+			// another client has initiated the manifest generation
+			// just wait 10s for the manifest file to be ready
+			return setTimeout(function() {
+				if (!fs.existsSync('www/webm_live/' + self.name + '/glass_live_manifest.mpd'))
+					return callback({'message':'Failed to generate manifest'});
+				callback(null);
+			}, 10000);
+		}
+		self.deleteFolderRecursive('www/webm_live/' + self.name); // this folder is not expected to exist. assert instead?
+		fs.mkdir('www/webm_live/' + self.name, function(error) {
+			if (error)
+				return callback(error);
+			// run first ffmpeg command to generate chunks and header files, this process will keep running
+			// to generate chunks continuously.
+			// TODO: set a timeout and kill this process when no more clients request for chunk files.
+			self.dashProcess = child_process.spawn("ffmpeg", [ '-rtsp_transport', 'tcp', '-i', 'rtsp://192.168.1.4:554/11',
+				'-map', '0:0', '-c:v', 'libvpx-vp9', '-keyint_min', '40', '-g', '40', '-r', '20', '-speed', '6', '-tile-columns', '4',
+				'-frame-parallel', '1', '-threads', '8', '-static-thresh', '0', '-max-intra-rate', '300', '-deadline', 'realtime',
+				'-lag-in-frames', '0', '-error-resilient', '1', '-f', 'webm_chunk', '-header', 'www/webm_live/' + self.name + '/glass_360.hdr',
+				'-chunk_start_index', '1', 'www/webm_live/' + self.name + '/glass_360_%d.chk','-map', '0:1', '-c:a', 'libvorbis', '-f', 'webm_chunk',
+				'-audio_chunk_duration', '2000', '-header', 'www/webm_live/' + self.name + '/glass_171.hdr', '-chunk_start_index', '1',
+				'www/webm_live/' + self.name + '/glass_171_%d.chk' ], {
+				detached: false
+			});
+			self.dashProcess.stderr.on('data', function(data) {
+				//console.log('dashProcess.stderr: ', data.toString());
+			});
+			self.dashProcess.stdout.on('data', function(data) {
+				//console.log('dashProcess.stdout: ', data.toString());
+			});
+			self.dashProcess.on('error', function(error) {
+				self.terminateDash();
+			});
+			self.dashProcess.on('exit', function(code, signal) {
+				self.terminateDash();
+			});
+			// wait 10s for header files to be generated as these are the required inuput for the second ffmpeg command
+			setTimeout(function() {
+				if (!fs.existsSync('www/webm_live/' + self.name + '/glass_360.hdr'))
+					return callback({'message':'Failed to generate headers'});
+				// run second ffmpeg command to generate manifest. this command exits immediately after manifest is generated
+				self.dashManifestProcess = child_process.spawn("ffmpeg", [ '-f', 'webm_dash_manifest', '-live', '1',
+					'-i', 'www/webm_live/' + self.name + '/glass_360.hdr', '-f', 'webm_dash_manifest', '-live', '1', '-i',
+					'www/webm_live/' + self.name + '/glass_171.hdr', '-c', 'copy', '-map', '0', '-map', '1', '-f', 'webm_dash_manifest',
+					'-live', '1', '-adaptation_sets', 'id=0,streams=0 id=1,streams=1', '-chunk_start_index', '1',
+					'-chunk_duration_ms', '2000', '-time_shift_buffer_depth', '7200', '-minimum_update_period', '7200',
+					'www/webm_live/' + self.name + '/glass_live_manifest.mpd' ], {
+					detached: false
+				});
+				self.dashManifestProcess.stderr.on('data', function(data) {
+					console.log('dashManifestProcess.stderr: ', data.toString());
+				});
+				self.dashManifestProcess.stdout.on('data', function(data) {
+					console.log('dashManifestProcess.stdout:', data.toString());
+				});
+				self.dashManifestProcess.on('error', function(error) {
+					self.terminateDash();
+					callback(error);
+				});
+				// second ffmpeg command is expected to exit when the manifest file has been generated.
+				// check for success exit code (0) and invoke the callback to return the manifest to client.
+				self.dashManifestProcess.on('exit', function(code, signal) {
+					// check self.dashManifestProcess as  the 'error' event above may have fired and callback already called
+					if (!self.dashManifestProcess)
+						return;
+					if (code === 0)
+						return callback(null); // success
+					// failure: exit code != 0
+					self.terminateDash();
+					callback({'message':'Failed to generate manifest'});
+				});
+			}, 10000);
+			setTimeout(self.checkCleanupDash.bind(self), 60000);
+		});
+	}
+	
+	// update dashChunkRequested flag that the checkCleanupDash timer checks every interval 
+	checkDashRequest(clientReq) {
+		if (clientReq.url.match('^/www/webm_live/' + this.name + '/.*?\.chk$'))
+			this.dashChunkRequested = true;
+	}
+	
+	// if no requests come in for webm chunk when timer expires then kill ffmpeg dash process and cleanup
+	checkCleanupDash() {
+		if (!this.dashChunkRequested)
+			return this.terminateDash();
+		this.dashChunkRequested = false; // reset flag
+		setTimeout(this.checkCleanupDash.bind(this), 60000); // and check again in 1 minute
+	}
+	
+	// kill the ffmpeg process that generates webm chunks and delete the entire output folder
+	terminateDash() {
+		if (this.dashManifestProcess) {
+			this.dashManifestProcess.kill('SIGTERM');
+			delete this.dashManifestProcess;
+		}
+		if (this.dashProcess) {
+			this.dashProcess.kill('SIGTERM');
+			delete this.dashProcess;
+		}
+		this.deleteFolderRecursive('www/webm_live/' + this.name);
 	}
 }
 
